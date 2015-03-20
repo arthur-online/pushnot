a228c66fce56a325af82cc9e3ec895855436c16c
arthur-online <arthur-online@users.noreply.github.com>
2015-03-21 02:00:33 +0530

    Update PushNotification.js

--- PushNotification.js
+++ PushNotification.js
@@ -1,140 +1,35 @@
-    function onDeviceReady() {
-                alert('Device is ready');
-                try 
-                { 
-                    pushNotification = window.plugins.pushNotification;
-              //$("#app-status-ul").append('<li>registering ' + device.platform + '</li>');
-              alert('Registering ' + device.platform);
-       if (device.platform == 'android' || device.platform == 'Android') {
-                        pushNotification.register(
-                        successHandler, 
-                        errorHandler, 
-                        {
-                            "senderID":984949831491,
-                            "ecb":"onNotificationGCM"
-                        });     // required!
+var app = {
+init: function() {
+    document.addEventListener("deviceready", this.onDeviceReady, false);
+    },
 
-                        alert('Registered the Android device');
-                        alert('regID = ' + e.regid);
-                    } else {
-                        pushNotification.register(tokenHandler, errorHandler, {"badge":"true","sound":"true","alert":"true","ecb":"onNotificationAPN"});    // required!
-                        alert('Registered the iOS device');
-                    }
-                }
-                catch(err) 
-                { 
-                    txt="There was an error on this page.\n\n"; 
-                    txt+="Error description: " + err.message + "\n\n"; 
-                    //alert(txt); 
-                    alert('Error: ' + err.message);
-                } 
-            }
+    onDeviceReady: function(){
+       // DO STUFF
+       // ....
 
-            // handle APNS notifications for iOS
-            function onNotificationAPN(e) {
-                if(e.alert) {
-                     //$("#app-status-ul").append('<li>push-notification: ' + e.alert + '</li>');
-                     //alert('push-notification: ' + e.alert);
-                     // showing an alert also requires the org.apache.cordova.dialogs plugin
-                     navigator.notification.alert(e.alert);
-                }
+       // ENABLE PUSH
+       this.push_init();
+    },
 
-                if(e.sound) {
-                    // playing a sound also requires the org.apache.cordova.media plugin
-                    var snd = new Media(e.sound);
-                    snd.play();
-                }
+    push_init: function(){
+        app.SENDER_ID = 123456789; // replaced by my actual GCM project no
 
-                if(e.badge) {
-                    pushNotification.setApplicationIconBadgeNumber(successHandler, e.badge);
-                }
-            }
+        var pushNotification = window.plugins.pushNotification;
 
-            // handle GCM notifications for Android
-            function onNotificationGCM(e) {
-                //$("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
-                alert('EVENT -> RECEIVED:' + e.event);
+        pushNotification.register( 
+            function(){alert('Push: win');}, // never called
+            function(){alert('Push: Error');},  // never called
+            { senderID: app.SENDER_ID, ecb: "app.push_android" }
+        );
+    },
+   // never called
+   push_android: function(e){
+       alert('connection established...');
+   console.log( 'successfully started android' );
+   console.log( e );
+   }
 
-                switch( e.event )
-                {
-                    case 'registered':
-                    if( e.regid.length > 0 )
-                    {
-                        //$("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
-                        // Your GCM push server needs to know the regID before it can push to this device
-                        // here is where you might want to send it the regID for later use.
-                        console.log("regID = " + e.regid);
-                        alert(' REGID = ' + e.regid);
-                    }
-                    break;
+};
 
-                    case 'message':
-                        // if this flag is set, this notification happened while we were in the foreground.
-                        // you might want to play a sound to get the user's attention, throw up a dialog, etc.
-                        if(e.foreground)
-                        {
-                            //$("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
-                            alert('--INLINE NOTIFICATION--');
-
-                                // on Android soundname is outside the payload. 
-                                    // On Amazon FireOS all custom attributes are contained within payload
-                                    var soundfile = e.soundname || e.payload.sound;
-                                    // if the notification contains a soundname, play it.
-                                    // playing a sound also requires the org.apache.cordova.media plugin
-                                    var my_media = new Media("/android_asset/www/"+ soundfile);
-                            my_media.play();
-                        }
-                        else
-                        {   // otherwise we were launched because the user touched a notification in the notification tray.
-                            if(e.coldstart)
-                                //$("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
-                                alert('--COLDSTART NOTIFICATION--')
-                            else
-                            //$("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
-                                alert('--BACKGROUND NOTIFICATION--')
-                        }
-
-                        //$("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
-                        alert('MESSAGE -> MSG: ' + e.payload.message);
-                        //android only
-                        //$("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
-                        alert('MESSAGE -> MSGCNT: ' + e.payload.msgcnt);
-                        //amazon-fireos only
-                        //$("#app-status-ul").append('<li>MESSAGE -> TIMESTAMP: ' + e.payload.timeStamp + '</li>');
-                        alert('MESSAGE -> TIMESTAMP: ' + e.payload.timeStamp);
-                    break;
-
-                    case 'error':
-                        //$("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
-                        alert('ERROR -> MSG' + e.msg);
-                    break;
-
-                    default:
-                        //$("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
-                        alert('EVENT -> Unknown, an event was received and we do not know what it is');
-                    break;
-                }
-            }
-
-            function tokenHandler (result) {
-                //$("#app-status-ul").append('<li>token: '+ result +'</li>');
-                // Your iOS push server needs to know the token before it can push to this device
-                // here is where you might want to send it the token for later use.
-                alert('iOS Result = ' + result);
-            }
-
-            function successHandler (result) {
-                //$("#app-status-ul").append('<li>success:'+ result +'</li>');
-                alert('Android Result = ' + result);
-                alert('RegID = ' + e.regid);
-            }
-
-            function errorHandler (error) {
-                //$("#app-status-ul").append('<li>error:'+ error +'</li>');
-                alert('Error = ' + error);
-            }
-
-            document.addEventListener('deviceready', onDeviceReady(), true);
-
-alert('regID = ' + e.regid);            
-alert('Reg code completed');
+// start the app
+app.init();
