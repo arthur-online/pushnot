6db99addf89c12a89ee71a6aeb1d74f6b1f15a58
arthur-online <arthur-online@users.noreply.github.com>
2015-03-20 22:31:42 +0530

    Update pushNotification.js

--- pushNotification.js
+++ pushNotification.js
@@ -5,7 +5,7 @@
                 try 
                 { 
                             var pushNotification
-                    pushNotification = window.plugins.pushNotification;
+                    pushNotification = window.plugins.PushNotification;
               //$("#app-status-ul").append('<li>registering ' + device.platform + '</li>');
               alert('Registering ' + device.platform);
                     if(device.platform == 'android' || device.platform == 'Android' ||device.platform == 'amazon-fireos' ) {
