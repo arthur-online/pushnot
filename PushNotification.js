683f0a24087cfd911d48a7291fbfcbafcc671fa5
arthur-online <arthur-online@users.noreply.github.com>
2015-03-23 02:08:11 +0530

    Update PushNotification.js

--- PushNotification.js
+++ PushNotification.js
@@ -1,147 +1,150 @@
-// Init the plugin
-var PushNotification = function () {
+
+
+
+    function onDeviceReady() {
+                alert('Device is ready');
+                try 
+                { 
+                  pushNotification.install = function () {
+if (!window.plugins) {
+window.plugins = {};
 }
-// Types
-PushNotification.prototype.notificationType = {
-none: 0,
-badge: 1,
-sound: 2,
-alert: 4
-}
-// Helpers
-PushNotification.prototype.failure = function (msg) {
-console.log("Javascript Callback Error: " + msg)
-}
-PushNotification.prototype.call_native = function (callback, name, args) {
-if(arguments.length == 2) {
-args = []
-}
-ret = cordova.exec(
-callback, // called when signature capture is successful
-this.failure, // called when signature capture encounters an error
-'PushNotificationPlugin', // Tell cordova that we want to run "PushNotificationPlugin"
-name, // Tell the plugin the action we want to perform
-args); // List of arguments to the plugin
-return ret;
-}
-PushNotification.prototype.isPlatformIOS = function () {
-return device.platform == "iPhone" || device.platform == "iPad" || device.platform == "iPod touch" || device.platform == "iOS"
-}
-// Core API
-// Registration
-PushNotification.prototype.registerForNotificationTypes = function (types, callback) {
-if(device.platform == "iPhone" || device.platform == "iPad" || device.platform == "iPod touch" || device.platform == "iOS") {
-this.call_native(callback, "registerForNotificationTypes", [types])
-}
-}
-// Top level enabling/disabling
-PushNotification.prototype.enablePush = function (callback) {
-this.call_native(callback, "enablePush");
-}
-PushNotification.prototype.disablePush = function (callback) {
-this.call_native(callback, "disablePush");
-}
-PushNotification.prototype.enableLocation = function (callback) {
-this.call_native(callback, "enableLocation")
-}
-PushNotification.prototype.disableLocation = function (callback) {
-this.call_native(callback, "disableLocation")
-}
-PushNotification.prototype.enableBackgroundLocation = function (callback) {
-this.call_native(callback, "enableBackgroundLocation")
-}
-PushNotification.prototype.disableBackgroundLocation = function (callback) {
-this.call_native(callback, "disableBackgroundLocation")
-}
-// is* functions
-PushNotification.prototype.isPushEnabled = function (callback) {
-this.call_native(callback, "isPushEnabled");
-}
-PushNotification.prototype.isSoundEnabled = function (callback) {
-if(device.platform == "Android") {
-this.call_native(callback, "isSoundEnabled");
-}
-}
-PushNotification.prototype.isVibrateEnabled = function (callback) {
-if(device.platform == "Android") {
-this.call_native(callback, "isVibrateEnabled");
-}
-}
-PushNotification.prototype.isQuietTimeEnabled = function (callback) {
-this.call_native(callback, "isQuietTimeEnabled");
-}
-PushNotification.prototype.isInQuietTime = function (callback) {
-this.call_native(callback, "isInQuietTime");
-}
-PushNotification.prototype.isLocationEnabled = function (callback) {
-this.call_native(callback, "isLocationEnabled");
-}
-PushNotification.prototype.isBackgroundLocationEnabled = function (callback) {
-this.call_native(callback, "isBackgroundLocationEnabled");
-}
-// Getters
-PushNotification.prototype.getIncoming = function (callback) {
-this.call_native(callback, "getIncoming");
-}
-PushNotification.prototype.getPushID = function (callback) {
-this.call_native(callback, "getPushID")
-}
-PushNotification.prototype.getQuietTime = function (callback) {
-this.call_native(callback, "getQuietTime");
-}
-PushNotification.prototype.getTags = function (callback) {
-this.call_native(callback, "getTags");
-}
-PushNotification.prototype.getAlias = function (callback) {
-this.call_native(callback, "getAlias");
-}
-// Setters
-PushNotification.prototype.setAlias = function (alias, callback) {
-this.call_native(callback, "setAlias", [alias])
-}
-PushNotification.prototype.setTags = function (tags, callback) {
-this.call_native(callback, "setTags", [tags])
-}
-PushNotification.prototype.setSoundEnabled = function (bool, callback) {
-if(device.platform == "Android") {
-this.call_native(callback, "setSoundEnabled", [bool])
-}
-}
-PushNotification.prototype.setVibrateEnabled = function (bool, callback) {
-if(device.platform == "Android") {
-this.call_native(callback, "setVibrateEnabled", [bool])
-}
-}
-PushNotification.prototype.setQuietTimeEnabled = function (bool, callback) {
-this.call_native(callback, "setQuietTimeEnabled", [bool])
-}
-PushNotification.prototype.setQuietTime = function (startHour, startMinute, endHour, endMinute, callback) {
-this.call_native(callback, "setQuietTime", [startHour, startMinute, endHour, endMinute])
-}
-PushNotification.prototype.setAutobadgeEnabled = function (enabled, callback) {
-if (this.isPlatformIOS()) {
-this.call_native(callback, "setAutobadgeEnabled", [enabled]);
-}
-}
-PushNotification.prototype.setBadgeNumber = function (number, callback) {
-if (this.isPlatformIOS()) {
-this.call_native(callback, "setBadgeNumber", [number]);
-}
-}
-// Reset Badge
-PushNotification.prototype.resetBadge = function (callback) {
-if (this.isPlatformIOS()) {
-this.call_native(callback, "resetBadge");
-}
-}
-// Clear notifications
-PushNotification.prototype.clearNotifications = function (callback) {
-if (!this.isPlatformIOS()) {
-this.call_native(callback, "clearNotifications");
-}
-}
-// Location stuff
-PushNotification.prototype.recordCurrentLocation = function (callback) {
-this.call_native(callback, "recordCurrentLocation");
-}
-module.exports = new PushNotification();
+window.plugins.pushNotification = new pushNotification();
+return window.plugins.pushNotification;
+};
+                    pushNotification = window.plugins.pushNotification;
+              //$("#app-status-ul").append('<li>registering ' + device.platform + '</li>');
+              alert('Registering ' + device.platform);
+       if (device.platform == 'android' || device.platform == 'Android') {
+                        pushNotification.register(
+                        successHandler, 
+                        errorHandler, 
+                        {
+                            "senderID":"984949831491",
+                            "ecb":"onNotificationGCM"
+                        });     // required!
+
+                        alert('Registered the Android device');
+                        alert('regID = ' + e.regid);
+                    } else {
+                        pushNotification.register(tokenHandler, errorHandler, {"badge":"true","sound":"true","alert":"true","ecb":"onNotificationAPN"});    // required!
+                        alert('Registered the iOS device');
+                    }
+                }
+                catch(err) 
+                { 
+                    txt="There was an error on this page.\n\n"; 
+                    txt+="Error description: " + err.message + "\n\n"; 
+                    //alert(txt); 
+                    alert('Error: ' + err.message);
+                } 
+            }
+
+            // handle APNS notifications for iOS
+            function onNotificationAPN(e) {
+                if(e.alert) {
+                     //$("#app-status-ul").append('<li>push-notification: ' + e.alert + '</li>');
+                     //alert('push-notification: ' + e.alert);
+                     // showing an alert also requires the org.apache.cordova.dialogs plugin
+                     navigator.notification.alert(e.alert);
+                }
+
+                if(e.sound) {
+                    // playing a sound also requires the org.apache.cordova.media plugin
+                    var snd = new Media(e.sound);
+                    snd.play();
+                }
+
+                if(e.badge) {
+                    pushNotification.setApplicationIconBadgeNumber(successHandler, e.badge);
+                }
+            }
+
+            // handle GCM notifications for Android
+            function onNotification(e) {
+                //$("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
+                alert('EVENT -> RECEIVED:' + e.event);
+
+                switch( e.event )
+                {
+                    case 'registered':
+                    if( e.regid.length > 0 )
+                    {
+                        //$("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
+                        // Your GCM push server needs to know the regID before it can push to this device
+                        // here is where you might want to send it the regID for later use.
+                        console.log("regID = " + e.regid);
+                        alert(' REGID = ' + e.regid);
+                    }
+                    break;
+
+                    case 'message':
+                        // if this flag is set, this notification happened while we were in the foreground.
+                        // you might want to play a sound to get the user's attention, throw up a dialog, etc.
+                        if(e.foreground)
+                        {
+                            //$("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
+                            alert('--INLINE NOTIFICATION--');
+
+                                // on Android soundname is outside the payload. 
+                                    // On Amazon FireOS all custom attributes are contained within payload
+                                    var soundfile = e.soundname || e.payload.sound;
+                                    // if the notification contains a soundname, play it.
+                                    // playing a sound also requires the org.apache.cordova.media plugin
+                                    var my_media = new Media("/android_asset/www/"+ soundfile);
+                            my_media.play();
+                        }
+                        else
+                        {   // otherwise we were launched because the user touched a notification in the notification tray.
+                            if(e.coldstart)
+                                //$("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+                                alert('--COLDSTART NOTIFICATION--')
+                            else
+                            //$("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+                                alert('--BACKGROUND NOTIFICATION--')
+                        }
+
+                        //$("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+                        alert('MESSAGE -> MSG: ' + e.payload.message);
+                        //android only
+                        //$("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+                        alert('MESSAGE -> MSGCNT: ' + e.payload.msgcnt);
+                        //amazon-fireos only
+                        //$("#app-status-ul").append('<li>MESSAGE -> TIMESTAMP: ' + e.payload.timeStamp + '</li>');
+                        alert('MESSAGE -> TIMESTAMP: ' + e.payload.timeStamp);
+                    break;
+
+                    case 'error':
+                        //$("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+                        alert('ERROR -> MSG' + e.msg);
+                    break;
+
+                    default:
+                        //$("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+                        alert('EVENT -> Unknown, an event was received and we do not know what it is');
+                    break;
+                }
+            }
+
+            function tokenHandler (result) {
+                //$("#app-status-ul").append('<li>token: '+ result +'</li>');
+                // Your iOS push server needs to know the token before it can push to this device
+                // here is where you might want to send it the token for later use.
+                alert('iOS Result = ' + result);
+            }
+
+            function successHandler (result) {
+                //$("#app-status-ul").append('<li>success:'+ result +'</li>');
+                alert('Android Result = ' + result);
+                alert('RegID = ' + e.regid);
+            }
+
+            function errorHandler (error) {
+                //$("#app-status-ul").append('<li>error:'+ error +'</li>');
+                alert('Error = ' + error);
+            }
+
+            document.addEventListener('deviceready', onDeviceReady(), true);
+
+alert('regID = ' + e.regid);            
+alert('Reg code completed');
